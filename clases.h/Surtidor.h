4a9dc8f7ef0cfe93d0223b5e06a9d950317744f8
AlexanderOsorio-dev <alexander.osorio2@udea.edu.co>
2024-10-10 19:38:04 -0500

    Se mejoraron los metodos de la clase
    Se agregaron sus getters y setters correctamente aparte de agregar funcionalidades extra

--- clases.h/Surtidor.h
+++ clases.h/Surtidor.h
@@ -7,16 +7,28 @@
 using namespace std;
 
 class Surtidor {
-private:
-    string codigo;
-    string modelo;
-    string estacionPerteneciente;
-    bool estado;
-public:
-    void reportarVenta(string tipoCombustible,unsigned short int cantidadCombustibleVendida);
-    void venderCombustible(string tipoCombustible, unsigned short int cantidadRequerida, unsigned short int cantidadDisponible);
-
-
-}
-
+    private:
+        string codigo;
+        string modelo;
+        string estacionPerteneciente;
+        bool estado;
+    public:
+    
+        Surtidor(string codigo_, string modelo_, string estacionPerteneciente_, bool estado_);
+        Surtidor();
+        ~Surtidor();
+        
+        string getCodigo() const;
+        string getModelo() const;
+        string getEstacionPerteneciente() const;
+        bool getEstado() const;
+        
+        void setCodigo(string nuevoCodigo);
+        void setModelo(string nuevoModelo);
+        void setEstacionPerteneciente(string nuevaEstacionPerteneciente);
+        void setEstado(bool nuevoEstado);
+        
+        void reportarVenta(string tipoCombustible,unsigned short int cantidadCombustibleVendida, unsigned short int costoCombustibleTotal);
+        void venderCombustible(string tipoCombustible, unsigned short int cantidadRequerida, unsigned short int cantidadDisponible);
+};
 #endif
