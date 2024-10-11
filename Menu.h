ca34d1785be7bd6fffcb5169617cdab4e578b7c4
JaiderBedoya <161373759+JaiderBedoya@users.noreply.github.com>
2024-10-10 19:52:39 -0500

    creación de menu.h
    donde están los prototipos del menú que redirecciona a las funcionalidades del sistema.

---
+++ Menu.h
@@ -0,0 +1,12 @@
+#ifndef MENU_H
+#define MENU_H
+#include <iostream>
+#include <string>
+#include "red.h"
+void menuRed(Red& redNacional);
+void menuEstacion();
+void verificarFugas();
+void simularVenta();
+void menu(Red& redNacional);
+void fijarprecios(Red& redNacional);
+#endif
