b12acf4388f51f5fc63aea66def7e28f7db077f1
AlexanderOsorio-dev <alexander.osorio2@udea.edu.co>
2024-10-10 19:42:04 -0500

    se agregaron definiciones de funciones nuevas

--- FuncionesAnexas.h
+++ FuncionesAnexas.h
@@ -1,16 +1,19 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 #include "Station.h"
+#include "Surtidor.h"
 #include <iostream>
 #include <string>
 #include <random>
 #include <fstream>
-using namespace std;
 
 Station leerDatosEstacion(const string datosEstacion);
 bool verificacionFuga(unsigned short int );
 unsigned short int contarCaracteresEnString(const string cadenaCaracteres, const string caracterParaContar);
 unsigned short int contarLineasEnArchivo(const string& nombreArchivo);
+void llenarArregloEstaciones(Station* arregloEstaciones, const string& nomArchivo);
+Surtidor leerDatosSurtidor(const string datosSurtidor);
+void llenarArregloSurtidores(Surtidor* arregloSurtidores, const string& nomArchivo);
 
 
 #endif
