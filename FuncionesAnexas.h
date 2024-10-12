3a9cf9a8178f82807b7c78b47a20a686c44617b6
JaiderBedoya <161373759+JaiderBedoya@users.noreply.github.com>
2024-10-11 21:48:35 -0500

    se agregaron cabeceras de esValidoFormatoGerente y esValidoFormatoUbicacion
    solo se agregaron sus cabeceras

--- FuncionesAnexas.h
+++ FuncionesAnexas.h
@@ -6,15 +6,17 @@
 #include <string>
 #include <random>
 #include <fstream>
-using namespace std;
+#include <ctype.h>
 
 Station leerDatosEstacion(const string datosEstacion);
-bool verificacionFuga(unsigned short int );
+bool verificacionFuga(unsigned short int);
 unsigned short int contarCaracteresEnString(const string cadenaCaracteres, const string caracterParaContar);
 unsigned short int contarLineasEnArchivo(const string& nombreArchivo);
 void llenarArregloEstaciones(Station* arregloEstaciones, const string& nomArchivo);
 Surtidor leerDatosSurtidor(const string datosSurtidor);
 void llenarArregloSurtidores(Surtidor* arregloSurtidores, const string& nomArchivo);
+bool esValidoFormatoGerente(const string& gerente);
+bool esValidoFormatoUbicacion(const std::string& ubicacion);
 string obtenerFechaActual();
 unsigned short int numeroAletatorio(unsigned short int minimo, unsigned short int maximo);
 
