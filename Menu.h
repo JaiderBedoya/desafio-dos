cb3107d60405fa371e25c30049be6b114f081394
JaiderBedoya <161373759+JaiderBedoya@users.noreply.github.com>
2024-10-11 21:41:54 -0500

    inclui funcionesAnexas
    inclui funcionesAnexas.h

--- Menu.h
+++ Menu.h
@@ -3,10 +3,13 @@
 #include <iostream>
 #include <string>
 #include "red.h"
+#include "Station.h"
+#include "FuncionesAnexas.h"
 void menuRed(Red& redNacional);
 void menuEstacion();
 void verificarFugas();
 void simularVenta();
 void menu(Red& redNacional);
-void fijarprecios(Red& redNacional);
+void fijarPrecios(Red& redNacional);
+void agregarEstación();
 #endif
