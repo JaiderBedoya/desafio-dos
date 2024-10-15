44297be09f212c50bf4262de5e4812216a75495e
AlexanderOsorio-dev <alexander.osorio2@udea.edu.co>
2024-10-14 23:51:55 -0500

    Se agregaron declaraciones de funciones nuevas

--- FuncionesAnexas.h
+++ FuncionesAnexas.h
@@ -2,22 +2,29 @@
 #define FUNCIONES_H
 #include "Station.h"
 #include "Surtidor.h"
+#include "Red.h"
 #include <iostream>
 #include <string>
 #include <random>
 #include <fstream>
 #include <ctype.h>
+using namespace std;
 
 Station leerDatosEstacion(const string datosEstacion);
-bool verificacionFuga(unsigned short int);
+bool verificacionFuga(unsigned short int );
 unsigned short int contarCaracteresEnString(const string cadenaCaracteres, const string caracterParaContar);
 unsigned short int contarLineasEnArchivo(const string& nombreArchivo);
 void llenarArregloEstaciones(Station* arregloEstaciones, const string& nomArchivo);
 Surtidor leerDatosSurtidor(const string datosSurtidor);
 void llenarArregloSurtidores(Surtidor* arregloSurtidores, const string& nomArchivo);
-bool esValidoFormatoGerente(const string& gerente);
-bool esValidoFormatoUbicacion(const std::string& ubicacion);
 string obtenerFechaActual();
 unsigned short int numeroAletatorio(unsigned short int minimo, unsigned short int maximo);
+void escribirDatosSurtidoresEnArchivo(Surtidor* arregloSurtidores, unsigned short int tamanioArreglo);
+void escribirDatosEstacionesEnArchivo(Station* arregloEstaciones, unsigned short int tamanioArreglo);
+unsigned short int obtenerIdxSurtidorEliminar(Surtidor* arregloSurtidores, string& codigoSurtidorAEliminar, unsigned short int tamanioArreglo);
+unsigned short int obtnerIdxEstacionEliminar(Station* arregloEstaciones, string& codigoEstacionAEliminar, unsigned short int tamanioArreglo);
+bool esValidoFormatoGerente(const string& gerente);
+bool esValidoFormatoUbicacion(const std::string& ubicacion);
+short int encontrarStringEnArreglo(string* arregloStrings, unsigned short int tamanioArreglo, string& stringAEncontrar);
 
 #endif
