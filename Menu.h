247dafb00811ba787fa8648bb1b813b134be7ebd
AlexanderOsorio-dev <alexander.osorio2@udea.edu.co>
2024-10-15 23:39:39 -0500

    Se mejoraron funciones existentes

--- Menu.h
+++ Menu.h
@@ -6,9 +6,8 @@
 #include "Station.h"
 #include "FuncionesAnexas.h"
 
-void menu(unsigned short int& tamanioArregloSurtidores, unsigned short int& tamanioArregloEstaciones, Station* estacionesEnELArchivo, Surtidor* surtidoresEnELArchivo, Red& redNacionalColombia);
-void agregarEstacion(Red& redNacional, Station* estacionesEnELArchivo, unsigned short int tamanioArregloEstaciones);
-void eliminarSurtidor(Surtidor* surtidoresEnELArchivo, unsigned short int& tamanioArregloSurtidores, Station& estacionAdministrada);
+void menu();
+Station pedirDatosEstacion();
 void fijarPrecios(Red& redNacional);
 void menuRed(Red& redNacional, Station* arregloEstaciones, unsigned short int& tamanioArregloEstaciones);
 void menuEstacion();
@@ -16,6 +15,6 @@ void verificarFugas();
 void simularVenta();
 void activarDesactivarSurtidor(bool activar, unsigned short int& tamanioArregloSurtidores, Surtidor* surtidoresEnELArchivo, Station* estacionAdministrada);
 void consultarHistoricoSurtidor(Surtidor* surtidoresEnELArchivo, unsigned short int& tamanioArregloSurtidores, Station& estacionAdministrada);
-
+void menuDefinitivo();
 
 #endif
